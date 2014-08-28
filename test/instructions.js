1d873ccac1d7135451f961a1c19ab9260860a996
SomeKittens <rkoutnik@gmail.com>
2014-08-27 18:02:25 -0700

    major refactor of tests

---
+++ test/instructions.js
@@ -0,0 +1,164 @@
+'use strict';
+
+/**
+ * Tests for specific Dockerfile instructions
+ */
+
+var validateDockerfile = require('../');
+
+function expectsSuccess (line) {
+  return function () {
+    var dockerfile = ['FROM vader/death-star',
+      line,
+      'CMD ["destroy", "Yavin IV"]'].join('\n');
+
+      var result = validateDockerfile(dockerfile);
+
+      result.should.be.an.Object;
+      result.should.have.property('valid', true);
+      result.should.not.have.property('message');
+      result.should.not.have.property('line');
+  };
+}
+
+function expectsFailure (line) {
+  return function () {
+    var dockerfile = ['FROM vader/death-star',
+    line,
+    'CMD ["destroy", "Yavin IV"]'].join('\n');
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(1);
+    result.errors[0].should.have.property('message', 'Bad parameters');
+    result.errors[0].should.have.property('line', 2);
+  };
+}
+
+describe('required instructions', function () {
+  describe('from', function () {
+    it('Should take a standard FROM', expectsSuccess(''));
+
+    it('Should reject with capital letters', function () {
+      var dockerfile = 'FROM Vader/Death-Star\nCMD ["destroy", "Yavin IV"]';
+
+      var result = validateDockerfile(dockerfile);
+
+      result.should.be.an.Object;
+      result.should.have.property('valid', false);
+      result.should.have.property('errors');
+      result.errors.should.be.an.Array;
+      result.errors.length.should.eql(1);
+      result.errors[0].should.have.property('message', 'Bad parameters');
+      result.errors[0].should.have.property('line', 1);
+    });
+  });
+
+  describe('cmd', function () {
+    it('Should take a standard CMD', expectsSuccess(''));
+
+    it.skip('Should reject with invalid parameters');
+  });
+});
+
+describe('optional instructions', function () {
+  describe('maintainer', function () {
+    it('Should take a standard MAINTAINER', expectsSuccess(
+      'MAINTAINER Grand Moff Tarkin <wtarkin@empire.com>'
+    ));
+
+    it('Should reject with no parameters', expectsFailure(
+      'MAINTAINER'
+    ));
+  });
+
+  describe('expose', function () {
+    it('Should take a standard EXPOSE', expectsSuccess(
+      'EXPOSE 80'
+    ));
+
+    it('allows multiple ports', expectsSuccess(
+      'EXPOSE 80 443'
+    ));
+
+    // See, if the Imperials had used unit testing, they'd be much better off
+    it('Should reject with letters', expectsFailure(
+      'EXPOSE exhaust port'
+    ));
+  });
+
+  describe('env', function () {
+    it('Should take a standard ENV', expectsSuccess(
+      'ENV battlestation fully_armed'
+    ));
+
+    it('allows multiple characters', expectsSuccess(
+      'ENV tk_421 absent'
+    ));
+
+    it('Should reject with no second parameter',  expectsFailure(
+      'ENV stand_by'
+    ));
+  });
+
+  describe('user', function () {
+    it('Should take a standard USER', expectsSuccess(
+      'USER not_r2'
+    ));
+
+    it('Rejects a username starting with a number', expectsFailure(
+      'USER 1138'
+    ));
+
+    it('Should reject a username with > 31 characters', expectsFailure(
+      'USER RUARRRRRRRRRRRRRRRRRRRRRRRRRRRRRGH'
+    ));
+  });
+
+  // Skipping these until we have actual regexes
+  describe.skip('run', function() {});
+  describe.skip('onbuild', function() {});
+  describe.skip('entrypoint', function() {});
+
+  describe('add', function () {
+    it('Should take a standard filepath ADD', expectsSuccess(
+      'ADD ./tie-fighter /hangar'
+    ));
+
+    it('Should take a standard url ADD', expectsSuccess(
+      'ADD http://superlaser.com /Alderaan'
+    ));
+
+    it('Rejects a malformed URL', expectsFailure(
+      'ADD htp://superlaser.com /Alderaan'
+    ));
+
+    it('Rejects a ADD with one parameter', expectsFailure(
+      'ADD ./tie-fighter'
+    ));
+  });
+
+  describe('volume', function () {
+    it('Should take a standard VOLUME', expectsSuccess(
+      'VOLUME ./1.72x10tothe7thpower'
+    ));
+
+    it('Should take a JSON array VOLUME', expectsSuccess(
+      'VOLUME ["./1.72x10tothe7thpower"]'
+    ));
+
+    it('Rejects improper JSON', expectsFailure(
+      'VOLUME ["1.72'
+    ));
+  });
+
+  describe('workdir', function () {
+    it('Should take a standard WORKDIR', expectsSuccess(
+      'WORKDIR ./Despayre'
+    ));
+  });
+});
---
+++ test/main.js
@@ -0,0 +1,154 @@
+'use strict';
+
+require('should');
+
+var validateDockerfile = require('../')
+  , find = require('find')
+  , fs = require('fs')
+  , path = require('path')
+  , callbackCount = require('callback-count');
+
+// dockerfiles tested here from https://github.com/kstaken/dockerfile-examples/tree/master/salt-minion
+describe('valid dockerfiles', function () {
+  it('should approve all valid dockerfiles', function (done) {
+    var counter = callbackCount(done);
+    find.eachfile(/.*[0-9]$/, path.join(__dirname, 'Dockerfiles'), function (file) {
+      counter.inc();
+
+      fs.readFile(file, 'UTF-8', function (err, data) {
+        if (err) {
+          throw err;
+        }
+        var isValid = validateDockerfile(data);
+        if (!isValid.valid) {
+          counter.next(new Error(isValid[0].message));
+        }
+        counter.next();
+      });
+    });
+  });
+});
+
+describe('invalid dockerfiles', function () {
+  it('should complain about an invalid instruction', function () {
+    var dockerfile = 'FROM vader/force-powers\nCONJURE stolen_data_tapes';
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(2);
+    result.errors[0].should.have.property('message', 'Invalid instruction');
+    result.errors[0].should.have.property('line', 2);
+    result.errors[1].should.have.property('message', 'Missing CMD');
+    result.errors[1].should.not.have.property('line');
+  });
+
+  it('should complain about bad parameters', function () {
+    var dockerfile = 'FROM Incom/Z-95 Headhunter';
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(2);
+    result.errors[0].should.have.property('message', 'Bad parameters');
+    result.errors[0].should.have.property('line', 1);
+    result.errors[1].should.have.property('message', 'Missing CMD');
+    result.errors[1].should.not.have.property('line');
+  });
+
+  it('should flunk a file with no FROM', function () {
+    var dockerfile = 'Hi mom!';
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(3);
+    result.errors[0].should.have.property('message', 'Missing FROM');
+    result.errors[0].should.have.property('line', 1);
+    result.errors[1].should.have.property('message', 'Invalid instruction');
+    result.errors[1].should.have.property('line', 1);
+    result.errors[2].should.have.property('message', 'Missing CMD');
+    result.errors[2].should.not.have.property('line');
+  });
+
+  it('should flunk a file with no CMD', function () {
+    var dockerfile = 'FROM wedge/rogue-squadron';
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(1);
+    result.errors[0].should.have.property('message', 'Missing CMD');
+    result.errors[0].should.not.have.property('line');
+  });
+
+  describe('empty', function () {
+    it('rejects an empty string', function () {
+      var dockerfile = '';
+
+      var result = validateDockerfile(dockerfile);
+
+      result.should.be.an.Object;
+      result.should.have.property('valid', false);
+      result.should.have.property('errors');
+      result.errors.should.be.an.Array;
+      result.errors.length.should.eql(1);
+      result.errors[0].should.have.property('message', 'Empty dockerfile');
+      result.errors[0].should.not.have.property('line');
+    });
+
+    it('rejects a dockerfile with only comments', function () {
+      var dockerfile = '#Witness the power of this\n#Fully documented battlestation';
+
+      var result = validateDockerfile(dockerfile);
+
+      result.should.be.an.Object;
+      result.should.have.property('valid', false);
+      result.should.have.property('errors');
+      result.errors.should.be.an.Array;
+      result.errors.length.should.eql(1);
+      result.errors[0].should.have.property('message', 'Empty dockerfile');
+      result.errors[0].should.not.have.property('line');
+    });
+
+    it('rejects a dockerfile with only newlines', function () {
+      var dockerfile = '\n\n\n\n\n';
+
+      var result = validateDockerfile(dockerfile);
+
+      result.should.be.an.Object;
+      result.should.have.property('valid', false);
+      result.should.have.property('errors');
+      result.errors.should.be.an.Array;
+      result.errors.length.should.eql(1);
+      result.errors[0].should.have.property('message', 'Empty dockerfile');
+      result.errors[0].should.not.have.property('line');
+    });
+  });
+
+  it('rejects non-string dockerfiles', function () {
+    var dockerfile = ['a long time ago', 'in a galaxy far far away'];
+
+    var result = validateDockerfile(dockerfile);
+
+    result.should.be.an.Object;
+    result.should.have.property('valid', false);
+    result.should.have.property('errors');
+    result.errors.should.be.an.Array;
+    result.errors.length.should.eql(1);
+    result.errors[0].should.have.property('message', 'Invalid type');
+    result.errors[0].should.not.have.property('line');
+  });
+});
